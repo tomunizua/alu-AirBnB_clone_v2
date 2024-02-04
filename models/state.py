8c2dbfdaa5e73f1dfb91fc2734ed5c33f0c7f3a2
Josiane Ishimwe <127387467+Joh-Ishimwe@users.noreply.github.com>
2024-02-04 16:38:05 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""This is the state class"""
+""" State Module for HBNB project"""
+
+
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
-import models
+from sqlalchemy.orm import relationship
+from models.engine.file_storage import FileStorage
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
+    """ State class definition in the next line"""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
+    cities = relationship('City', backref='state', cascade="all, delete")
 
-    @property
-    def cities(self):
-        """ Returns the list of City instances with
-        state_id == current State.id """
-        list_cities = []
-        for city in models.storage.all(City).values():
-            if self.id == city.state_id:
-                list_cities.append(city)
-        return list_cities
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
+        @property
+        def cities(self):
+            from models import storage
+            from models.city import City
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
