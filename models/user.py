6b815b0e577a8c5fcb52825bed43f80fb413b9a3
Josiane Ishimwe <127387467+Joh-Ishimwe@users.noreply.github.com>
2024-02-04 16:37:00 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""This is the user class"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from models.place import Place
-from models.review import Review
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for your login
-        first_name: first name
-        last_name: last name
-    """
+    """This class defines a user by various attributes"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", cascade="all, delete", backref="user")
-    reviews = relationship("Review", cascade="all, delete", backref="user")
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    places = relationship('Place', backref='user', cascade='delete')
+    reviews = relationship('Review', backref='user', cascade='delete')
