66e6b7eb68bb817599f3e092bb3dd91acd7f48f8
Josiane Ishimwe <127387467+Joh-Ishimwe@users.noreply.github.com>
2024-02-07 14:12:07 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project"""
-
-
+"""This is the state class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.engine.file_storage import FileStorage
-from os import getenv
+from models.city import City
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy.orm import relationship, backref
+import models
+from os import environ
 
 
 class State(BaseModel, Base):
-    """ State class definition in the next line"""
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade="all, delete")
 
-    if getenv("HBNB_TYPE_STORAGE") != 'db':
+    if environ.get('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+
         @property
         def cities(self):
-            from models import storage
-            from models.city import City
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in list(storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """ Returns the list of City instances with
+            state_id == current State.id """
+            all_cities = models.storage.all(City)
+            state_cities = []
+            for city_ins in all_cities.values():
+                if city_ins.state_id == self.id:
+                    state_cities.append(city_ins)
+
+            return state_cities
